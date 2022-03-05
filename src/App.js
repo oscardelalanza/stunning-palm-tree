cd7ff1e7afd40e4c46dfb3e1c2a2c8059c4a72c9
oscardelalanza <oscardelalanza@gmail.com>
2022-03-05 03:46:28 -0800

    Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { Routes, Route, Link, Switch } from 'react-router-dom'
+
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Start</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+        </ul>
+      </nav>
+      <section>
+        <Routes>
+          <Route exact path="/portfolio" element={<h1>Portfolio</h1>}/>
+          <Route exact path="/" element={<h1>Start</h1>}/>
+        </Routes>
+      </section>
+    </div>
   )
 }
 
