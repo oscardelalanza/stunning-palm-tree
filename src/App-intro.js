e95a871687f5d60bb40fe55b5f43fb87519df304
oscardelalanza <oscardelalanza@gmail.com>
2022-03-05 04:16:44 -0800

    nested routes

---
+++ src/App-intro.js
@@ -0,0 +1,26 @@
+import { Routes, Route, Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Start</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+        </ul>
+      </nav>
+      <section>
+        <Routes>
+          <Route exact path="/portfolio" element={<h1>Portfolio</h1>}/>
+          <Route exact path="/" element={<h1>Start</h1>}/>
+        </Routes>
+      </section>
+    </div>
+  )
+}
+
+export default App
--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
-import { Routes, Route, Link, Switch } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <ul>
+        <li>
+          <Link to="/portfolio/project-1">Project 1</Link>
+        </li>
+        <li>
+          <Link to="/portfolio/project-2">Project 2</Link>
+        </li>
+      </ul>
+      <div>
+        <Routes>
+          <Route path="/project-1" element={<h2>Project 1</h2>}/>
+          <Route path="/project-2" element={<h2>Project 2</h2>}/>
+        </Routes>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -15,8 +37,8 @@ function App() {
       </nav>
       <section>
         <Routes>
-          <Route exact path="/portfolio" element={<h1>Portfolio</h1>}/>
-          <Route exact path="/" element={<h1>Start</h1>}/>
+          <Route path="/portfolio/*" element={<Portfolio />} />
+          <Route exact path="/" element={<h1>Start</h1>} />
         </Routes>
       </section>
     </div>
